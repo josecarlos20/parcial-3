6de37bcc4ed9eec078aba22fe01d5e673af9032a
josecarlos20 <78504539+josecarlos20@users.noreply.github.com>
2022-05-24 21:43:52 -0500

    Update jugador.py

--- jugador.py
+++ jugador.py
@@ -2,4 +2,11 @@ public class Persona extends jugador{
     public String nombre;
     public String nif;
     public String fechaNAC;
+    
+    def __init__(self, numfed):
+    self.numfed=numfed
+    
+    def set numfed():
+    input("ingrese el numero que corresponde")
+    
 
